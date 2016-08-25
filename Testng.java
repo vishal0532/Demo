c9793d96f6a94208d401ea673ebe3eeb2afb1d97
Vishal Naik <vishal_naik@persistent.co.in>
2016-08-25 13:06:19 +0530

    testing

---
+++ Testng.java
@@ -0,0 +1,32 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testng {
+	Logging empBusinessLogic = new Logging();
+	   Emp employee = new Emp();
+
+	   @Test
+	   public void testCalculateAppriasal() {
+	   
+	      employee.setName("Rajeev");
+	      employee.setAge(25);
+	      employee.setMonthlySalary(8000);
+	      	
+	      double appraisal = empBusinessLogic.calculateAppraisal(employee);
+	      Assert.assertEquals(500, appraisal, 0.0, "500");
+	   }
+
+	   // Test to check yearly salary
+	   @Test
+	   public void testCalculateYearlySalary() {
+	   
+	      employee.setName("Rajeev");
+	      employee.setAge(25);
+	      employee.setMonthlySalary(8000);
+	      
+	      double salary = empBusinessLogic.calculateYearlySalary(employee);
+	      Assert.assertEquals(96000, salary, 0.0, "8000");
+	   }
+	}
